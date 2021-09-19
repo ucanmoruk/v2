4982c50056f3de21bc45bc21a95c6d2f154fd4a9
ucanmoruk <oguzhanneker@gmail.com>
2021-09-19 18:54:27 +0300

    Dx

--- StokTakip/Analiz/ValidasyonPlanListesi.cs
+++ StokTakip/Analiz/ValidasyonPlanListesi.cs
@@ -30,25 +30,41 @@ namespace StokTakip.Analiz
             //    " inner join StokAnalizListesi s on v.AnalizID = s.ID inner join StokDKDListe d on s.Metot = d.ID " +
             //    " left join ValidasyonYetkili y on v.AnalizID = y.AnalizID left join StokKullanici k on y.PersonelID = k.ID where v.Durum = 'Plan'  and y.Durum = 'Plan'", bgl.baglanti());
 
-            SqlDataAdapter da2 = new SqlDataAdapter("select v.ID, s.Kod + ' ' + s.Ad as 'Analiz', d.Kod + ' ' + d.Ad as 'Metot Kaynağı', v.Tarih1 as 'Başlangıç',  " +
-                "v.Tarih2 as 'Bitiş',  v.Urun as 'Matriks', v.Aciklama as 'Açıklama' ,  v.AnalizID as 'AnalizID' , v.Durumu from  ValidasyonVeri v  " +
-                "inner join StokAnalizListesi s on v.AnalizID = s.ID inner join StokDKDListe d on s.Metot = d.ID where v.Durum = 'Plan' or v.Durum = 'Ortak' ", bgl.baglanti());
+            //SqlDataAdapter da2 = new SqlDataAdapter("select v.ID, s.Kod + ' ' + s.Ad as 'Analiz', d.Kod + ' ' + d.Ad as 'Metot Kaynağı', v.Tarih1 as 'Başlangıç',  " +
+            //    "v.Tarih2 as 'Bitiş',  v.Urun as 'Matriks', v.Aciklama as 'Açıklama' ,  v.AnalizID as 'AnalizID' , v.Durumu from  ValidasyonVeri v  " +
+            //    "inner join StokAnalizListesi s on v.AnalizID = s.ID inner join StokDKDListe d on s.Metot = d.ID where v.Durum = 'Plan' or v.Durum = 'Ortak' ", bgl.baglanti());
+            //da2.Fill(dt2);
+            SqlDataAdapter da2 = new SqlDataAdapter(@"
+            SELECT DISTINCT(l.Kod), l.Ad, v.Urun, v.Tarih1,
+            STUFF((SELECT ',' + listek.Ad + ' ' + listek.Soyad AS [text()]
+                FROM ValidasyonVeri listev
+                left join StokAnalizListesi listel on listev.AnalizID = listel.ID
+		        left join ValidasyonYetkili listey on listev.AnalizID = listey.AnalizID
+		        left join StokKullanici listek on listey.PersonelID = listek.ID
+		        where listel.Kod = l.Kod
+                FOR XML PATH('')
+                ), 1, 1, '' )
+            AS list
+        FROM  ValidasyonVeri v
+        left join StokAnalizListesi l on v.AnalizID = l.ID
+        left join ValidasyonYetkili y on v.AnalizID = y.AnalizID
+        left join StokKullanici k on y.PersonelID = k.ID", bgl.baglanti());
             da2.Fill(dt2);
             gridControl1.DataSource = dt2;
 
-            gridView1.Columns["ID"].Visible = false;
-            gridView1.Columns["AnalizID"].Visible = false;
-            this.gridView1.Columns[1].Width = 100;
-            this.gridView1.Columns[2].Width = 170;
-            this.gridView1.Columns[3].Width = 40;
-            this.gridView1.Columns[4].Width = 40;
-            this.gridView1.Columns[5].Width = 80;
-            this.gridView1.Columns[6].Width = 150;
-            this.gridView1.Columns[7].Width = 70;
-          //  this.gridView1.Columns[8].Width = 70;
+          //  gridView1.Columns["ID"].Visible = false;
+          //  gridView1.Columns["AnalizID"].Visible = false;
+          //  this.gridView1.Columns[1].Width = 100;
+          //  this.gridView1.Columns[2].Width = 170;
+          //  this.gridView1.Columns[3].Width = 40;
+          //  this.gridView1.Columns[4].Width = 40;
+          //  this.gridView1.Columns[5].Width = 80;
+          //  this.gridView1.Columns[6].Width = 150;
+          //  this.gridView1.Columns[7].Width = 70;
+          ////  this.gridView1.Columns[8].Width = 70;
 
-            RepositoryItemMemoEdit memo = new RepositoryItemMemoEdit();
-            gridView1.Columns["Açıklama"].ColumnEdit = memo;
+            //RepositoryItemMemoEdit memo = new RepositoryItemMemoEdit();
+            //gridView1.Columns["Açıklama"].ColumnEdit = memo;
         }
 
         private void barButtonItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
@@ -121,10 +137,10 @@ namespace StokTakip.Analiz
         string vID, AnalizID, durum;
         private void gridView1_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
         {
-            DataRow dr = gridView1.GetDataRow(gridView1.FocusedRowHandle);
-            vID = dr["ID"].ToString();
-            AnalizID = dr["AnalizID"].ToString();
-            durum = dr["Durumu"].ToString();
+            //DataRow dr = gridView1.GetDataRow(gridView1.FocusedRowHandle);
+            //vID = dr["ID"].ToString();
+            //AnalizID = dr["AnalizID"].ToString();
+            //durum = dr["Durumu"].ToString();
         }
 
         private void gridView1_PopupMenuShowing(object sender, DevExpress.XtraGrid.Views.Grid.PopupMenuShowingEventArgs e)
@@ -166,22 +182,22 @@ namespace StokTakip.Analiz
        
         private void gridView1_RowCellStyle(object sender, DevExpress.XtraGrid.Views.Grid.RowCellStyleEventArgs e)
         {
-            if (e.Column.FieldName == "Başlangıç" || e.Column.FieldName == "Durumu" || e.Column.FieldName == "Bitiş")
-                e.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            //if (e.Column.FieldName == "Başlangıç" || e.Column.FieldName == "Durumu" || e.Column.FieldName == "Bitiş")
+            //    e.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
 
-            GridView View = sender as GridView;
-            if (e.RowHandle >= 0)
-            {
-                string ODurum = View.GetRowCellDisplayText(e.RowHandle, View.Columns["Durumu"]);
-                if (ODurum == "İptal Edildi")
-                {
-                    e.Appearance.BackColor = Color.MediumVioletRed;
-                }
-                else if (ODurum == "Gerçekleşti")
-                {
-                    e.Appearance.BackColor = Color.LightGreen;
-                }
-            }
+            //GridView View = sender as GridView;
+            //if (e.RowHandle >= 0)
+            //{
+            //    string ODurum = View.GetRowCellDisplayText(e.RowHandle, View.Columns["Durumu"]);
+            //    if (ODurum == "İptal Edildi")
+            //    {
+            //        e.Appearance.BackColor = Color.MediumVioletRed;
+            //    }
+            //    else if (ODurum == "Gerçekleşti")
+            //    {
+            //        e.Appearance.BackColor = Color.LightGreen;
+            //    }
+            //}
         }
 
         private void barButtonItem5_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
